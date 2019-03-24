5f996d6e00150dbe25a65cf1856ef2e67aa9dce8
axel92b <axel92b@gmail.com>
2019-03-24 19:04:18 +0200

    deleted useless code, added mising label, added event on press "Enter" key, more fixes to multithreading

--- gui.py
+++ gui.py
@@ -9,62 +9,7 @@ import datetime
 #TODO validate minutes input
 #TODO add labels
 #TODO clean code
-#TODO try to fix errors on exit
 
-class Calculator:
-
-    def __init__(self, master):
-        self.master = master
-        master.title("Calculator")
-
-        self.total = 0
-        self.entered_number = 0
-
-        self.total_label_text = IntVar()
-        self.total_label_text.set(self.total)
-        self.total_label = Label(master, textvariable=self.total_label_text)
-
-        self.label = Label(master, text="Total:")
-
-        vcmd = master.register(self.validate) # we have to wrap the command
-        self.entry = Entry(master, validate="key", validatecommand=(vcmd, '%P'))
-
-        self.add_button = Button(master, text="+", command=lambda: self.update("add"))
-        self.subtract_button = Button(master, text="-", command=lambda: self.update("subtract"))
-        self.reset_button = Button(master, text="Reset", command=lambda: self.update("reset"))
-
-        # LAYOUT
-
-        self.label.grid(row=0, column=0, sticky=W)
-        self.total_label.grid(row=0, column=1, columnspan=2, sticky=E)
-
-        self.entry.grid(row=1, column=0, columnspan=3, sticky=W+E)
-
-        self.add_button.grid(row=2, column=0)
-        self.subtract_button.grid(row=2, column=1)
-        self.reset_button.grid(row=2, column=2, sticky=W+E)
-
-    def validate(self, new_text):
-        if not new_text: # the field is being cleared
-            self.entered_number = 0
-            return True
-
-        try:
-            self.entered_number = int(new_text)
-            return True
-        except ValueError:
-            return False
-
-    def update(self, method):
-        if method == "add":
-            self.total += self.entered_number
-        elif method == "subtract":
-            self.total -= self.entered_number
-        else: # reset
-            self.total = 0
-
-        self.total_label_text.set(self.total)
-        self.entry.delete(0, END)
 
 class GetGrades:
     def __init__(self, master , login, passw, sem):
@@ -74,8 +19,8 @@ class GetGrades:
         self.passw = passw
         self.count = 0
         self.threadMessage = 1
-        self.textCouter = ""
         self.courseLabel = Label(self.master, text= "Course")
+        self.durationLabel = Label(self.master, text= "Duration")
         master.title("Get Grades(Alpha) - Hi " + self.login)
         self.a = []
         self.b = []
@@ -95,12 +40,11 @@ class GetGrades:
         self.updButt = Button(self.master, text = "Monitor!", command = self.updateFunc)
 
         # LAYOUT
-        self.courseLabel.grid(column = 0, row = 0)
+        self.courseLabel.grid(column = 0, row = 0,sticky = W)
+        self.durationLabel.grid(column = 0, row = 1,sticky = W)
         self.courseMenu.grid(column = 1, row = 0)
         self.minutes.grid(column = 1, row = 1)
         self.updButt.grid(column = 1, row = 2)
-
-    
 
     def getUpdatedIndexes(self,arr1,arr2):
         temp = []
@@ -120,21 +64,20 @@ class GetGrades:
         for i in range(2,len(arr)+2):
             targArr[i-2].grid(column = i, row = r)
 
-    def updateFunc(self):
+    def updateFunc(self, event = ""):
         self.killThread()
         self.threadMessage = 1
         self.t1 = threading.Thread(target=self.monFunc, args=[])
         self.t1.start()
 
-    def killThread(self):
+    def killThread(self, destroy=0):
+        if destroy == 1:
+            self.master.destroy()
         try:
             if self.t1.isAlive():
                 self.threadMessage = 0
-                # self.t1.join(10)
                 self.t1.join()
-        except NameError:
-            self.t1 = ""
-        except AttributeError:
+        except:
             self.t1 = ""
 
     def destroyLabel(self, arr):
@@ -162,10 +105,10 @@ class GetGrades:
             self.lArr4 = []
             self.lArr5 = []
             self.lArr6 = []
-            #DEBUG
             self.d,self.e,self.f = GetGradesClass.getData(self.login,self.passw, self.sem, self.saveCourseNum)
+            #DEBUG
             #self.d,self.e,self.f = next(kek)
-            if self.d != self.a or self.e != self.b or self.f != self.c or self.firstTime == 1:
+            if self.d != self.a or self.firstTime == 1:
                 self.count = 0
                 tempCompArr = self.getUpdatedIndexes(self.a, self.d)
                 self.fillLabels(self.d,self.lArr4,tempCompArr)
@@ -197,7 +140,6 @@ class GetGrades:
                 self.master.title("Get Grades(Alpha) - Hi " + self.login + " - Checked " + str(self.count) + " times")
             time.sleep(60*self.minutesVal)
             
-
 class LoginWin:
     def __init__(self, master):
         self.master = master
@@ -207,7 +149,7 @@ class LoginWin:
         self.l2 = Label(master, text="Password")
         self.login = Entry(master, bd = 5)
         self.passw = Entry(master, bd = 5, show = "*")
-        self.butt = Button(master, text="Login", command = lambda: self.tryToLogin(""))
+        self.butt = Button(master, text="Login", command = self.tryToLogin)
         self.yearVal = StringVar(self.master)
         self.yearVal.set(self.tempDate.year)
         self.year = Spinbox(self.master,from_ = self.tempDate.year-2, to = 2055 ,textvariable = self.yearVal, width = 4)
@@ -234,10 +176,12 @@ class LoginWin:
         else:
             return p1+'01'
 
-    def tryToLogin(self, event):
+    def tryToLogin(self, event = ""):
         self.newGui = Tk()
         newkek = GetGrades(self.newGui,self.login.get(),self.passw.get(),self.getDateFormat(self.yearVal.get(),self.chosenSem.get()))
         self.master.destroy()
+        self.newGui.bind("<Return>",newkek.updateFunc)
+        self.newGui.protocol("WM_DELETE_WINDOW", lambda: other.killThread(1))
         self.newGui.mainloop()
 
 
@@ -254,4 +198,6 @@ except FileNotFoundError:
 
 top = Tk()
 other = GetGrades(top,logs[0],logs[1],LoginWin.getDateFormat(None,'2018','Winter'))
+top.bind("<Return>",other.updateFunc)
+top.protocol("WM_DELETE_WINDOW", lambda: other.killThread(1))
 top.mainloop()
